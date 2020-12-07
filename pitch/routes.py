c9554a31c1783727896ad4737b05aac0b8ed71a5
ZephonMakale <mzephon@gmail.com>
2020-12-07 17:00:33 +0300

    Add the user functionality of creating pitches

--- pitch/routes.py
+++ pitch/routes.py
@@ -3,29 +3,15 @@ import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from pitch import app, db, bcrypt
-from pitch.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from pitch.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from pitch.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
-posts = [
-    {
-        'author': 'Zephon Makale',
-        'title': 'Pitch 1',
-        'content': 'I am an experienced Graphics Designer with 3 years of active work',
-        'date_posted': 'December 6th, 2020'
-    },
-    {
-        'author': 'B. Kamau',
-        'title': 'Pitch 2',
-        'content': 'I am an experienced Web Designer with 2 years of active work',
-        'date-posted': 'December 7th, 2020'
-    },
-
-]
 
 @app.route("/")
 @app.route("/home")
 def home():
+    posts = Post.query.all()
     return render_template('home.html', posts = posts)
 
 @app.route("/about")
@@ -98,3 +84,16 @@ def account():
         form.email.data = current_user.email
     image_file = url_for('static', filename = 'profile_pics/' + current_user.image_file)
     return render_template('account.html', title = 'Account', image_file = image_file, form = form)
+
+
+@app.route("/post/new", methods = ['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title = form.title.data, content = form.content.data, author = current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created successfully!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title = 'New Post', form = form)
