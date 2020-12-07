9c4aefc4c843025ee4c91a0ca48bb5fa99a4f507
ZephonMakale <mzephon@gmail.com>
2020-12-07 12:44:22 +0300

    Add custom validation

--- pitch/__init__.py
+++ pitch/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'a9e5c966dbd643ddca1c4249c0036039'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from pitch import routes
