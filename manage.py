4ba9eab5140e4f8332ce4877b73284aed83285cd
ZephonMakale <mzephon@gmail.com>
2020-12-08 21:55:45 +0300

    deployment to heroku2

--- manage.py
+++ manage.py
@@ -18,7 +18,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app =app,db = db, User = User, Post =Post, Upvote = Upvote, Downvote = Downvote)
+    return dict(app =app,db = db, User = User, Post =Post)
 
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
